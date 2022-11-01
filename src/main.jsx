548842b6d4dd4de01d6dee310eac99cf016670bf
alexgy <88888888@gmail.com>
2022-11-01 22:44:40 +0800

    init project

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+
+function App() {
+  return <div> hello 123123</div>;
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
